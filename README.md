869797d0858ee3845802b58f25e9fa9f1c5543a2
Jenn M <40043504+Jenn122783@users.noreply.github.com>
2022-11-03 10:33:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# portfolio_project
+HTML/CSS/JavaScript/JQuery used in the creation of this porfolio site project. 
+Final project for HTML/CSS II in the software development projram at Spokane Community College. 
+We were instructed to create a personal porfolio site to showcase our ability to create a functioning site with what we learned in class. 
---
+++ js/scrolling.js
@@ -0,0 +1,21 @@
+﻿
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 125
+    });
+
+    // smooth scrolling
+    $('nav a, .down-button').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1300, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+});
